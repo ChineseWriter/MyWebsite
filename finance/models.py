cbb4059979fcbdd3c496d0e9a3a1ba02dedcaddd
ThinkPadT430 <ChinaWhEnglish@outlook.com>
2022-02-10 12:06:30 +0800

    为finance应用创建基本模型。

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,70 @@
-from django.db import models
+# coding = UTF-8
 
-# Create your models here.
+from django.db import models
+from django.contrib import admin
+
+
+class ItemCategory(models.Model):
+    category = models.CharField(max_length=10, verbose_name="仓库物品的类别")
+
+    def __str__(self):
+        return self.category
+
+    class Meta:
+        verbose_name = "物品类别"
+
+
+class InventoryItems(models.Model):
+    name = models.CharField(max_length=50, verbose_name="仓库物品的名称")
+    category = models.ManyToManyField(ItemCategory, verbose_name="仓库物品的类别")
+    model = models.CharField(max_length=100, verbose_name="仓库物品的型号")
+    brand = models.CharField(max_length=50, verbose_name="仓库物品的品牌")
+    unit = models.CharField(max_length=5, verbose_name="仓库物品的单位")
+
+    @admin.display(boolean=True, description="仓库物品的类别")
+    def show_category(self, obj):
+        return ",".join([category.category for category in obj.category.all()])
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "仓库物品信息"
+
+
+class Procurement(models.Model):
+    procurement_time = models.DateTimeField(verbose_name="采购时间")
+    goods = models.ForeignKey(InventoryItems, models.PROTECT, verbose_name="仓库物品")
+    quantity = models.IntegerField(verbose_name="采购数量")
+    unit_price = models.FloatField(verbose_name="物品单价")
+
+    @admin.display(boolean=True, description="物品总价")
+    def show_total_price(self, obj):
+        return obj.quantity + obj.unit_price
+
+    def __str__(self):
+        return self.goods.name
+
+    class Meta:
+        verbose_name = "采购记录"
+
+
+class InventoryOperation(models.Model):
+    PUT_IN_STORAGE = "PIS"
+    CHECK_OUT_STORAGE = "COS"
+    OPERATION_CHOICES = [
+        (PUT_IN_STORAGE, "入库"),
+        (CHECK_OUT_STORAGE, "出库")
+    ]
+
+    operation_time = models.DateTimeField(verbose_name="操作时间")
+    operation = models.CharField(max_length=5, choices=OPERATION_CHOICES, verbose_name="操作")
+    goods = models.ForeignKey(InventoryItems, models.PROTECT, verbose_name="仓库物品")
+    quantity = models.IntegerField(verbose_name="操作数量")
+
+    def __str__(self):
+        return self.operation
+
+    class Meta:
+        verbose_name = "仓库操作记录"
+
---
+++ finance/signals.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :signals.py
+# @Time      :2022/2/6 11:33
+# @Author    :Amundsen Severus Rubeus Bjaaland
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
