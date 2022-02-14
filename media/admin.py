712e296745d7e9c460bc313a8c0b3bf49bc53760
ThinkPadT430 <ChinaWhEnglish@outlook.com>
2022-02-14 09:41:43 +0800

    为media应用创建音乐相关模型，并加入Django的后台。

--- media/admin.py
+++ media/admin.py
@@ -1,3 +1,58 @@
+# coding = UTF-8
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Poster, Singer, Music
+
+
+class PosterAdmin(admin.ModelAdmin):
+    list_per_page = 50
+    actions_on_top = True
+    actions_on_bottom = True
+    fieldsets = [
+        ("基本信息", {"fields": ["name", "source_site"]}),
+        ("其余信息", {"fields": ["self_source", "description"]}),
+        ("文件", {"fields": ["self_object"]})
+    ]
+    list_display = ["name", "source_site"]
+    list_filter = ["name"]
+    search_fields = ["name"]
+
+
+class SingerAdmin(admin.ModelAdmin):
+    list_per_page = 50
+    actions_on_top = True
+    actions_on_bottom = True
+    fieldsets = [
+        ("基本信息", {"fields": ["name", "source_site"]}),
+        ("其余信息", {"fields": ["description", "posters"]}),
+        ("编号信息", {"fields": ["master_id"]})
+    ]
+    list_display = ["name", "source_site"]
+    list_filter = ["name", "source_site"]
+    search_fields = ["name", "source_site"]
+
+
+class MusicAdmin(admin.ModelAdmin):
+    list_per_page = 100
+    actions_on_top = True
+    actions_on_bottom = True
+    fieldsets = [
+        ("基本信息", {"fields": ["name", "album", "source_site"]}),
+        ("其余信息", {"fields": ["lyrics", "posters", "singers"]}),
+        ("文件信息", {"fields": ["self_source", "self_object"]}),
+        ("MV信息", {"fields": ["mv_source", "mv"]}),
+        ("ID信息", {"fields": ["master_id", "sub_id"]}),
+    ]
+    list_display = ["name", "album", "source_site", "music_singers"]
+    list_filter = ["source_site"]
+    search_fields = ["name", "album", "source_site"]
+
+    @admin.display(description="歌曲演唱者")
+    def music_singers(self, obj):
+        return [one_singer.name for one_singer in obj.singers.all()]
+
+
+admin.site.register(Poster, PosterAdmin)
+admin.site.register(Singer, SingerAdmin)
+admin.site.register(Music, MusicAdmin)
--- media/models.py
+++ media/models.py
@@ -1,3 +1,55 @@
-from django.db import models
+# coding = UTF-8
 
-# Create your models here.
+
+from django.db import models
+from django.utils.translation import gettext_lazy
+
+
+class SupportWebsite(models.TextChoices):
+    KuGou = "KG", gettext_lazy("酷狗音乐")
+    WangYiYun = "WYY", gettext_lazy("网易云音乐")
+    QQMusic = "QM", gettext_lazy("QQ音乐")
+    BaiduPicture = "DP", gettext_lazy("百度图片")
+    BingPicture = "BP", gettext_lazy("必应图片")
+
+
+class Poster(models.Model):
+    name = models.CharField(max_length=20, verbose_name="海报名称")
+    source_site = models.CharField(max_length=5, verbose_name="来源网站", choices=SupportWebsite.choices)
+    self_source = models.URLField(verbose_name="海报来源")
+    self_object = models.ImageField(verbose_name="海报文件", upload_to="posters/%Y/%m/%d/")
+    description = models.TextField(verbose_name="海报描述", null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Singer(models.Model):
+    name = models.CharField(max_length=20, verbose_name="演唱者名称")
+    source_site = models.CharField(max_length=5, verbose_name="来源网站", choices=SupportWebsite.choices)
+    master_id = models.CharField(max_length=30, verbose_name="演唱者ID")
+    description = models.TextField(verbose_name="演唱者简介")
+    posters = models.ManyToManyField(Poster, verbose_name="演唱者海报")
+
+    def __str__(self):
+        return self.name
+
+
+class Music(models.Model):
+    name = models.CharField(max_length=30, verbose_name="歌曲名称")
+    source_site = models.CharField(max_length=5, verbose_name="来源网站", choices=SupportWebsite.choices)
+    self_source = models.URLField(verbose_name="歌曲来源")
+    self_object = models.FileField(verbose_name="歌曲文件", upload_to="musics/%Y/%m/%d/")
+    singers = models.ManyToManyField(Singer, verbose_name="歌曲演唱者")
+    posters = models.ManyToManyField(Poster, verbose_name="歌曲海报")
+    lyrics = models.TextField(verbose_name="歌曲歌词")
+    album = models.CharField(max_length=20, verbose_name="歌曲专辑")
+    mv = models.FileField(verbose_name="歌曲MV", upload_to="mv/%Y/%m/%d/", null=True, blank=True)
+    mv_source = models.URLField(verbose_name="MV来源", null=True, blank=True)
+    master_id = models.CharField(max_length=30, verbose_name="主ID")
+    sub_id = models.CharField(max_length=30, verbose_name="副ID")
+
+    def __str__(self):
+        return self.name
+
+
