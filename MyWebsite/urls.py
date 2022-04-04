deafa7919a0a861f9a5192edec6b10697a9d1f9d
闫鸿黎 <ChinaWhEnglish@outlook.com>
2022-04-04 10:25:22 +0800

    更新网站主页。

--- MyWebsite/urls.py
+++ MyWebsite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 
 
+import os
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -29,5 +31,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
