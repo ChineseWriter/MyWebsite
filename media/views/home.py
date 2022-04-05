2a951558fcf58256ffc343136a505e3aec381427
闫鸿黎 <ChinaWhEnglish@outlook.com>
2022-04-05 13:39:21 +0800

    初始化media应用新的主页。

--- media/views/home.py
+++ media/views/home.py
@@ -11,6 +11,19 @@ from django.views.decorators.http import require_GET
 from .. import forms
 
 
+APPNAME = "media"
+APPVERBOSENAME = "媒体文件下载器"
+DESCRIPTION = "主要用于从各大网站上下载指定的媒体文件，包括小说、图片、音乐、视频等"
+
+
 @require_GET
 def index(request):
-    return render(request, "media/index.html", {"MusicSelectForm": forms.home.MusicSelectForm()})
+    return render(
+        request, "media/index.html",
+        {
+            "MusicSelectForm": forms.home.MusicSelectForm(),
+            "AppName": APPNAME,
+            "AppVerboseName": APPVERBOSENAME,
+            "AppDesc": DESCRIPTION
+        }
+    )
